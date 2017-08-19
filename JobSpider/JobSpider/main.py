2a7298ae128fe47784bc2c507bcef5de11d5723d
yoyo@home <1197993367@qq.com>
2017-08-19 20:36:15 +0800

    4-5 完成了网页基本信息的提取，利用xpath

---
+++ JobSpider/JobSpider/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017-08-19 19:16
+# @Author  : YouSheng
+__author__ = 'YouSheng'
+
+#用此文件代替执行cmd命令行命令，有助于debug
+from scrapy.cmdline import execute
+import sys
+import os
+
+#设置cmd主目录，这样调用execute才会生效
+# sys.path.append('F:\PythonProjects\Scrapy_Job\JobSpider')
+sys.path.append(os.path.dirname(os.path.abspath(__file__))) #动态获取本文件的父文件夹路径
+
+execute(['scrapy','crawl','jobbole'])
--- JobSpider/JobSpider/spiders/jobbole.py
+++ JobSpider/JobSpider/spiders/jobbole.py
@@ -1,11 +1,39 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/112214/']
 
     def parse(self, response):
-        pass
+        title = response.xpath('//div[@class="entry-header"]/h1/text()').extract_first("")
+        create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace("·","").strip()
+        praise_nums = response.xpath("//span[contains(@class, 'vote-post-up')]/h10/text()").extract()[0]
+
+        fav_nums = response.xpath("//span[contains(@class, 'bookmark-btn')]/text()").extract()[0]
+        #过滤掉收藏数里的中文字
+        match_re = re.match(".*?(\d+).*", fav_nums)
+        if match_re:
+            fav_nums = match_re.group(1)
+
+        #评论数
+        comment_nums = response.xpath("//a[@href='#article-comment']/span/text()").extract()[0]
+        match_re = re.match(".*?(\d+).*", comment_nums)
+        if match_re:
+            comment_nums = match_re.group(1)
+
+        #正文
+        content = response.xpath("//div[@class='entry']").extract()[0]
+
+        tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
+        tag_list = [element for element in tag_list if not element.strip().endswith(u"评论")]
+        tags = ",".join(tag_list)
+        print title
+        print create_date
+        print praise_nums
+        print fav_nums
+        print comment_nums
+        print tags
