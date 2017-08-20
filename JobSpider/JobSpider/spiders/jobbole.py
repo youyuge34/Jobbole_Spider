e1fa5c0a3564ead0ca968cf6a14339cc4b2dd736
yoyo@home <1197993367@qq.com>
2017-08-20 10:18:52 +0800

    4-7 完成了网页基本信息的提取，利用css选择器

--- JobSpider/JobSpider/spiders/jobbole.py
+++ JobSpider/JobSpider/spiders/jobbole.py
@@ -24,6 +24,8 @@ class JobboleSpider(scrapy.Spider):
         match_re = re.match(".*?(\d+).*", comment_nums)
         if match_re:
             comment_nums = match_re.group(1)
+        else:
+            comment_nums = 0
 
         #正文
         content = response.xpath("//div[@class='entry']").extract()[0]
@@ -31,6 +33,33 @@ class JobboleSpider(scrapy.Spider):
         tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
         tag_list = [element for element in tag_list if not element.strip().endswith(u"评论")]
         tags = ",".join(tag_list)
+
+
+        #通过css选择器提取字段
+        # front_image_url = response.meta.get("front_image_url", "")  #文章封面图
+        # title = response.css(".entry-header h1::text").extract()[0]
+        # create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().replace("·","").strip()
+        # praise_nums = response.css(".vote-post-up h10::text").extract()[0]
+        # fav_nums = response.css(".bookmark-btn::text").extract()[0]
+        # match_re = re.match(".*?(\d+).*", fav_nums)
+        # if match_re:
+        #     fav_nums = int(match_re.group(1))
+        # else:
+        #     fav_nums = 0
+        #
+        # comment_nums = response.css("a[href='#article-comment'] span::text").extract()[0]
+        # match_re = re.match(".*?(\d+).*", comment_nums)
+        # if match_re:
+        #     comment_nums = int(match_re.group(1))
+        # else:
+        #     comment_nums = 0
+        #
+        # content = response.css("div.entry").extract()[0]
+        #
+        # tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract()
+        # tag_list = [element for element in tag_list if not element.strip().endswith(u"评论")]
+        # tags = ",".join(tag_list)
+
         print title
         print create_date
         print praise_nums
