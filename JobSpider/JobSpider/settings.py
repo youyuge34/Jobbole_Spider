d11327ea93cc53a87197250914bfe38ad9e1217d
yoyo@home <1197993367@qq.com>
2017-08-20 21:12:38 +0800

    NeetRaws模块蜘蛛，爬取Neet-Raws种子完成，测试ok

--- JobSpider/JobSpider/settings.py
+++ JobSpider/JobSpider/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'JobSpider.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'JobSpider (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
